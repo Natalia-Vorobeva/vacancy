946467b2e2f224442dd117c0c39d6b00a08b2119
Natalia <vorobjeva.natalia76@gmail.com>
2026-03-21 13:18:31 +0500

    Проверка бекенда минимальным запросом - 2 эндпоинта

--- server/main.py
+++ server/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional, List
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://vacancy-ten.vercel.app"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
+
+@app.get("/api/vacancies")
+def get_vacancies(query: str = Query("React"), page: int = Query(0)):
+    return [{"id": 1, "name": f"Test {query} page {page}"}]
